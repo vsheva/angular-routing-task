1b734df49e36966b10ba8f9322cff8428a7f74ae
vsheva <valera.sheva@gmail.com>
2023-12-01 17:09:43 -0500

    fetch route parameters using observable params

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,16 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Params} from "@angular/router";
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
+
+
 export class UserComponent implements OnInit {
   user: {id: number, name: string};
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+
+    this.user={
+      id: this.route.snapshot.params['id'], //!!
+      name:this.route.snapshot.params['name']
+    }
+
+    //!!! observable
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.user.id= params['id'];
+        this.user.name= params['name']
+      }
+    )
+
+
   }
 
 }
