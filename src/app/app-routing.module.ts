539ae56c7a7e16e4f5638b7bf82309791b5f960a
vsheva <valera.sheva@gmail.com>
2023-12-05 22:54:04 -0500

    protect routing with canActivate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,28 +8,33 @@ import {ServersComponent} from "./servers/servers.component";
 import {ServerComponent} from "./servers/server/server.component";
 import {EditServerComponent} from "./servers/edit-server/edit-server.component";
 import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
+import {AuthGuard} from "./auth-guard.service";
 
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'users', component: UsersComponent, children:[
+  {
+    path: 'users', component: UsersComponent, children: [
       {path: ':id/:name', component: UserComponent},
-    ]},
+    ]
+  },
 
-  {path: 'servers', component: ServersComponent, children:[
+  {
+    path: 'servers', canActivate: [AuthGuard], component: ServersComponent, children: [
       {path: ':id', component: ServerComponent}, //!!!
       {path: ':id/edit', component: EditServerComponent},
-    ]},
+    ]
+  },
   {path: 'not-found', component: PageNotFoundComponent},
-  { path: '', redirectTo: '/home', pathMatch: 'full' }, //!!!
+  {path: '', redirectTo: '/home', pathMatch: 'full'}, //!!!
   {path: '**', redirectTo: '/not-found'}
 ];
 
 
 @NgModule({
-imports:[
-  RouterModule.forRoot(appRoutes) //routs registered in app
-],
-  exports:[RouterModule] //!! export
+  imports: [
+    RouterModule.forRoot(appRoutes) //routs registered in app
+  ],
+  exports: [RouterModule] //!! export
 })
 
 export class AppRoutingModule {
